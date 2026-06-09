843bfbe1f0835a2f8488ca8816fa51b19486f718
unlsycn <unlsycn@unlsycn.com>
2026-06-09 13:52:51 +0800

    refactor(ally-z1e): use structured kernel config

--- pkgs/linux-ally-z1e/default.nix
+++ pkgs/linux-ally-z1e/default.nix
@@ -1,6 +1,11 @@
-{ lib, fetchFromGitHub, linuxManualConfig, ... }:
-{ ... } @args:
-
+{
+  lib,
+  fetchFromGitHub,
+  buildLinux,
+  writeText,
+  ...
+}:
+{ ... }@args:
 
 let
   inherit (lib) versions;
@@ -8,16 +13,68 @@ let
   kernelVersion = "6.16.12";
   vendorVersion = "valve7";
   hash = "sha256-x9moiht4YqVpg+048ol6NXW08OogUjAGAC8a6pd9Y3U=";
+
+  structuredConfig = import ./structured-config.nix { inherit lib; };
+  isDigit =
+    char:
+    lib.elem char [
+      "0"
+      "1"
+      "2"
+      "3"
+      "4"
+      "5"
+      "6"
+      "7"
+      "8"
+      "9"
+    ];
+  isNumeric = value: value != "" && lib.all isDigit (lib.stringToCharacters value);
+  renderFreeformSeed =
+    value: if isNumeric value || lib.hasPrefix "0x" value then value else builtins.toJSON value;
+  renderValue =
+    value:
+    if builtins.hasAttr "freeform" value then
+      renderFreeformSeed value.freeform
+    else if value.tristate == "y" then
+      "y"
+    else if value.tristate == "m" then
+      "m"
+    else if value.tristate == "n" then
+      "n"
+    else
+      null;
+  shouldCheckConfig =
+    name: value: name != "CC_CAN_LINK" && !(builtins.hasAttr "freeform" value && value.freeform == "");
+  checkedStructuredConfig = lib.filterAttrs shouldCheckConfig structuredConfig;
+  renderConfigLine =
+    name: value:
+    let
+      renderedValue = renderValue value;
+    in
+    if renderedValue == null then
+      ""
+    else if renderedValue == "n" then
+      "# CONFIG_${name} is not set\n"
+    else
+      "CONFIG_${name}=${renderedValue}\n";
+  configSeed = writeText "ally-z1e-config-seed" (
+    lib.concatStrings (lib.mapAttrsToList renderConfigLine structuredConfig)
+  );
 in
-(linuxManualConfig
-  (args // rec {
+(buildLinux (
+  args
+  // rec {
     version = "${kernelVersion}-${vendorVersion}";
 
     # branchVersion needs to be x.y
     extraMeta.branch = versions.majorMinor version;
 
-    configfile = ./ally-z1e-config;
-    allowImportFromDerivation = true;
+    defconfig = "KCONFIG_ALLCONFIG=${configSeed} allnoconfig";
+    enableCommonConfig = false;
+    autoModules = false;
+    ignoreConfigErrors = false;
+    structuredExtraConfig = checkedStructuredConfig;
 
     src = fetchFromGitHub {
       owner = "Jovian-Experiments";
@@ -38,13 +95,16 @@ in
         )
       '';
     };
-  } // (args.argsOverride or { }))).overrideAttrs (old: {
-  # https://github.com/NixOS/nixpkgs/issues/216529
-  # overrided passthru will be ignored by boot.kernelPackages
-  # hence we just override features
-  features = {
-    efiBootStub = true;
-    ia32Emulation = true;
-    netfilterRPFilter = false;
-  };
-})
+  }
+  // (args.argsOverride or { })
+)).overrideAttrs
+  (old: {
+    # https://github.com/NixOS/nixpkgs/issues/216529
+    # overrided passthru will be ignored by boot.kernelPackages
+    # hence we just override features
+    features = {
+      efiBootStub = true;
+      ia32Emulation = true;
+      netfilterRPFilter = false;
+    };
+  })
